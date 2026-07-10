6b78a90b180010b784a5aca55a8c4b5f99147342
Felipe Cornejo Cornejo <fvivancorne@gmail.com>
2026-07-10 11:35:07 -0400

    feat: Integrar Meta Pixel ID de TREKANWEB

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1111111111111111'); // TODO: REEMPLAZAR CON PIXEL ID REAL
+            fbq('init', '1351193506984796');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -112,7 +112,7 @@ export default function RootLayout({
         {/* Meta Pixel (noscript) */}
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-               src="https://www.facebook.com/tr?id=1111111111111111&ev=PageView&noscript=1"
+               src="https://www.facebook.com/tr?id=1351193506984796&ev=PageView&noscript=1"
           />
         </noscript>
         
