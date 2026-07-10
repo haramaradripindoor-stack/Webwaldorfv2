93bb2ccec51e1b2d1151774c9b47187abfa19c6c
Felipe Cornejo Cornejo <fvivancorne@gmail.com>
2026-07-10 11:36:38 -0400

    fix: Renombrar variable de Meta Pixel a NEXT_PUBLIC_FACEBOOK_PIXEL_ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1351193506984796');
+            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -112,7 +112,7 @@ export default function RootLayout({
         {/* Meta Pixel (noscript) */}
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-               src="https://www.facebook.com/tr?id=1351193506984796&ev=PageView&noscript=1"
+               src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
         
